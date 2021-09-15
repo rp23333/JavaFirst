4795cf50e7bd9019207a572a876b2ed9f1a1917d
rohanpatel <rohanpatel@10.1.67.85>
2021-09-15 13:20:48 -0400

    ma lksc

---
+++ Starter Project/src/IntroToJava/Variables.java
@@ -0,0 +1,107 @@
+package IntroToJava;
+
+public class Variables {
+	
+	public static void main(String[] args) {
+		
+		int x;
+		
+		x=9;
+		
+		System.out.println("I need some help with my math hw. if x = 9, then what's x+9?");
+		sleep(2500);
+		System.out.println();
+		
+		System.out.println("x+9=" + (x+9)+ ",You mindless fool!");
+		
+		System.out.println(4 * 3 / (2 + 3) );
+		
+		
+
+		
+
+
+	}
+		
+		
+		public static void sleep(int time) {
+			try {
+				Thread.sleep(time);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+	}
+	
+	
+
+}
+
+
+
+/*
+* 1. 99
+* 2. 15
+* 3. 8
+* 4. 5
+* 5. 5
+* 6. 7
+* 7. 7.5
+* 8. 0
+* 9. 0.5
+* 10. 1
+* 11. 3.5
+* 12. 0
+* 13. 3
+* 14. 4
+* 15. 23
+* 16. 32
+* 17. -22
+* 18. 1
+* 19. 2
+* 20. 11
+* 
+* 
+* 
+* 1. yes
+* 2. no
+* 3. yes
+* 4. yes
+* 5. yes
+* 6. no
+* 7. yes
+* 8. no
+* 
+* 
+* 1. yes
+* 2. no
+* 3. yes
+* 4. yes
+* 5. yes
+* 6. yes
+* 7. yes
+* 8. yes
+* 9. no
+* 10. no 
+* 
+* 
+* token: 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
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
