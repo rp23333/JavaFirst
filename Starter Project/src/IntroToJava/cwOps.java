decc38a4371e656c7b61d1d5c8debe2794a0994c
rohanpatel <rohanpatel@10.1.67.85>
2021-09-21 11:13:05 -0400

    f

--- Starter Project/src/IntroToJava/cwOps.java
+++ Starter Project/src/IntroToJava/cwOps.java
@@ -1,5 +1,33 @@
 package IntroToJava;
 
-public class cwOps {
 
+public class cwOps {
+	
+	public void questionOne(int Num1) {
+		
+		System.out.println((char)Num1);
+		
+	}
+	
+	public void questionTwo(int Num2){
+		
+		System.out.println(Num2%10);
+		
+	}
+	
+	public void questionThree(int Num4, int Num5) {
+		
+		System.out.println((Num4+Num5)/2);
+		
+	}
+	
+	
+	public static void main(String[] args) {
+		
+		cwOps runner = new cwOps();
+		runner.questionOne(106);
+		runner.questionTwo(16);
+		runner.questionThree(18,20);
+		
+	}
 }
